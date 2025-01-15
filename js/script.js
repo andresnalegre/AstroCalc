d74972e6d31fe707db8a9fc687aea7e3d7a66d01
Andres Nicolas <andresnicolasalegre@hotmail.com>
2025-01-15 14:15:04 +0000

    Moving files to the AstroCalc repository

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const buttons = document.querySelectorAll("button");
+const screenDisplay = document.querySelector(".screen");
+
+let currentInput = [];
+let calculationString = '';
+
+function calculate(button) {
+    const buttonValue = button.textContent;
+
+    if (buttonValue === "Clear") {
+        clearDisplay();
+    } else if (buttonValue === "=") {
+        evaluateCalculation();
+    } else {
+        appendToCalculation(buttonValue);
+    }
+}
+
+function clearDisplay() {
+    currentInput = [];
+    calculationString = '';
+    screenDisplay.value = '';
+}
+
+function appendToCalculation(value) {
+    currentInput.push(value);
+    calculationString = currentInput.join('');
+    screenDisplay.value = calculationString;
+}
+
+function evaluateCalculation() {
+    try {
+        const result = new Function(`return ${calculationString}`)();
+        screenDisplay.value = result;
+        currentInput = [result.toString()];
+    } catch (error) {
+        screenDisplay.value = "Error";
+        currentInput = [];
+    }
+}
+
+buttons.forEach(button => button.addEventListener("click", () => calculate(button)));
+
+let posX = 0, posY = 0;
+function moveBackground() {
+    posX += 0.01;
+    posY += 0.01;
+    document.body.style.backgroundPosition = `${posX}px ${posY}px`;
+    requestAnimationFrame(moveBackground);
+}
+moveBackground();
